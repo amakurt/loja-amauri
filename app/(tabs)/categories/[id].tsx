016f9342c9ec7f7641497519b61f549812053dae
Amauri Castro <87552742+amakurt@users.noreply.github.com>
2024-09-03 16:54:15 -0300

    modified the archive /categories/_layout.tsx and created file categories/[id].tsx

---
+++ app/(tabs)/categories/[id].tsx
@@ -0,0 +1,37 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { getAllProducts, getProductByCategory } from "../../../types/services/product";
+import { ProductItem } from "../../../components/product-item";
+import { router, Stack, useLocalSearchParams } from "expo-router";
+import { getCategoryById } from "../../../types/services/category";
+
+export default function Screen() {
+  const { id } = useLocalSearchParams();
+  const idCategory = parseInt(id as string);
+
+  const category = getCategoryById(idCategory);
+  if (!category) return router.back();
+
+  const products = getProductByCategory(idCategory);
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: category.title }} />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductItem data={item} />}
+        keyExtractor={item => item.id.toString()}
+        style={styles.list}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  list: {
+    width: '100%',
+    padding: 20
+  }
+})
